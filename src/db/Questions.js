72a0d13db2c474d57a4844eb27f631dc6bbff31d
PuruPanda1 <purabmodi2002@gmail.com>
2024-06-21 11:51:06 +0530

    Added update & delete functionality

--- src/db/Questions.js
+++ src/db/Questions.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
 import db from "../config/firebase.js";
 
 const questionDbReference = collection(db,"Questions");
@@ -19,4 +19,26 @@ export async function getQuestions(){
     const querySnapshot = await getDocs(questionDbReference);
     const questions = querySnapshot.docs.map((doc)=>({id:doc.id, ...doc.data()}));
     return questions;
+}
+
+export async function getQuestion(id){
+  const docRef = doc(db, "Questions", id);
+  const question = await getDoc(docRef);
+  console.log(question.exists());
+  if(question.exists()){
+    return question.data();
+  }
+  return {msg:"Not Found"};
+} 
+
+export async function updateQuestion(id, data){
+  const docRef = doc(db, "Questions", id);
+  await updateDoc(docRef, data)
+  return true;
+}
+
+export async function deleteQuestion(id){
+  const docRef = doc(db, "Questions", id);
+  await deleteDoc(docRef);
+  return true;
 }
--- src/routes/QuestionRouter.js
+++ src/routes/QuestionRouter.js
@@ -1,16 +1,37 @@
 import { Router } from "express";
-import { addQuestion, getQuestions } from "../db/Questions.js";
+import { addQuestion, deleteQuestion, getQuestion, getQuestions, updateQuestion } from "../db/Questions.js";
 
 const QuestionRouter = Router();
 
-QuestionRouter.get('/',async(req,res)=>{
+QuestionRouter.get('/get',async(req,res)=>{
     const data = await getQuestions();
+    res.send(data);
+})
+
+QuestionRouter.get('/get/:id',async(req,res)=>{
+    const id = req.params.id;
+    const data = await getQuestion(id);
+    console.log(data);
     res.send(data);
 })
 
 QuestionRouter.post('/add',async(req,res)=>{
     const id = await addQuestion(req.body);
     res.send({msg:`succcess with ${id}`});
+})
+
+QuestionRouter.post('/update',async(req,res)=>{
+    const id = req.body.id;
+    delete req.body.id;
+    const data = req.body;
+    await updateQuestion(id,data);
+    res.send("updated");
+})
+
+QuestionRouter.post('/delete',async(req,res)=>{
+    const id = req.body.id;
+    await deleteQuestion(id);
+    res.send({msg:"Deleted"});
 })
 
 export default QuestionRouter;
